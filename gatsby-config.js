3d322e42c9b849462d0c5642d2bc31e53c68a17e
Cassie Koomjian <cassie@substantial.com>
2019-04-06 13:27:25 -0700

    Remove manifest & offline plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,25 +43,6 @@ module.exports = {
     // configuration as well as setting up optimized server
     // rendering and client re-hydration.
     `gatsby-plugin-glamor`,
-    // This plugin takes your configuration and generates a
-    // web manifest file so Gatsbygram can be added to your
-    // homescreen on Android.
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsbygram`,
-        short_name: `Gatsbygram`,
-        start_url: `/`,
-        background_color: `#f7f7f7`,
-        theme_color: `#191919`,
-        display: `minimal-ui`,
-      },
-    },
-    // This plugin generates a service worker and AppShell
-    // html file so the site works offline and is otherwise
-    // resistant to bad networks. Works with almost any
-    // site!
-    `gatsby-plugin-offline`,
     // This plugin sets up Google Analytics for you.
     {
       resolve: `gatsby-plugin-google-analytics`,
